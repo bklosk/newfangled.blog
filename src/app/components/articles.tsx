0e4fe165d3a0140a38c7ce521d3df72c36a9e2cb
Ben Klosky <benklosky@uchicago.edu>
2024-12-31 01:24:35 -0500

    enhance Articles component with framer-motion animations and improve layout

--- src/app/components/articles.tsx
+++ src/app/components/articles.tsx
@@ -1,12 +1,29 @@
 "use client";
 import React from "react";
+import { motion } from "framer-motion";
 
-export default function Squares() {
+export default function Articles() {
   const squares = [];
 
   for (let i = 0; i < 10; i++) {
-    squares.push(<div key={i} className="w-48 h-48 border border-white"></div>);
+    squares.push(
+      <motion.div
+        key={i}
+        className="w-72 h-72 border border-white"
+        initial={{ opacity: 0, y: 40 }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          transition: { delay: 4 + i * 0.4, ease: "easeInOut", duration: 0.3 },
+        }}
+        drag
+        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+        whileHover={{ scale: 0.9, transition: { delay: 0 } }}
+      >
+        <motion.p>Article {i + 1}</motion.p>
+      </motion.div>
+    );
   }
 
-  return <div className="flex flex-wrap">{squares}</div>;
+  return <div className="flex flex-wrap mx-20">{squares}</div>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,14 @@ const gabarito = Gabarito({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="bg-gradient-to-br from-green-800 to-green-900 h-screen text-white">
+    <main className="bg-gradient-to-br from-green-800 to-green-900 h-full text-white">
       <Header />
       <div className={gabarito.className}>
         <motion.p
           initial={{ y: 20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ delay: 3, duration: 0.5 }}
-          className="text-3xl font-semibold ml-20"
+          className="text-3xl font-semibold ml-20 mb-8"
         >
           Articles
         </motion.p>
